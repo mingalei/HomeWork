f7764e768001cdc1726df9d965852bea137815d5
mingalei <mingalei33@gmail.com>
2023-07-29 14:25:13 +0300

    Initial commit

--- src/test/java/guru/qa/HomeWork2.java
+++ src/test/java/guru/qa/HomeWork2.java
@@ -3,7 +3,6 @@ package guru.qa;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
